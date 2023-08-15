8001c7902d39b12932abf307ea30e7264edeb82d
James Aubrey <jaaubrey92@gmail.com>
2023-08-15 10:40:50 -0500

    links to socials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ const Home = () => {
   return (
     <div>
       <h2>Amanda Aoki</h2>
+      <a href="https://www.instagram.com/amandaaaoki/">Instagram</a>
+      <a href="https://www.tiktok.com/@amandaaaoki">TikTok</a>
     </div>
   )
 }
