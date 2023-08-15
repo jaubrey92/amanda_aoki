9ac6d73f4e461d6450a4770a02f2a05fcf3bd256
James Aubrey <jaaubrey92@gmail.com>
2023-08-15 10:29:28 -0500

    Home, About Page, NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css'
+import Header from './components/Header'
+import Home from './pages/Home'
+import AboutMe from './pages/AboutMe'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,7 @@ const Header = () => {
   return (
     <header>
       <nav className="navStyle">
-        <Link to="/">Home</Link> |<Link to="/about">About Me</Link> |
-        <Link to="/portfolio">Portfolio</Link>
+        <Link to="/">Home</Link> |<Link to="/about">About Me</Link>
       </nav>
     </header>
   )
