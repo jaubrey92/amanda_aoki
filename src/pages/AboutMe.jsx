c638aa69de3dfa8e8ca6cc11f4356a7b290e9214
James Aubrey <jaaubrey92@gmail.com>
2023-08-21 18:28:42 -0500

    bio page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,17 +2,19 @@ const AboutMe = () => {
   return (
     <div className="container">
       <h2>Bio</h2>
-      <p>
-        Amanda Aoki is a Japanese American author who has always loved reading
-        and writing about strong girls who challenge systems of power. Give her
-        all the high stakes novels but a quiet, cozy life. A chronic relocator,
-        she currently resides in her home city in the San Francisco Bay Area
-        with her husband, daughter, and small flock of chickens. Amanda has
-        degrees in Psychology, Nursing, and Human Development and, much like Mia
-        Thermopolis, is a yoga doing, horse-back riding, wall climbing type of
-        girl. Amanda is represented by Jenniea Carter at New Leaf Literary
-        Agency
-      </p>
+      <div className="bookInfo">
+        <p>
+          Amanda Aoki is a Japanese American author who has always loved reading
+          and writing about strong girls who challenge systems of power. Give
+          her all the high stakes novels but a quiet, cozy life. A chronic
+          relocator, she currently resides in her home city in the San Francisco
+          Bay Area with her husband, daughter, and small flock of chickens.
+          Amanda has degrees in Psychology, Nursing, and Human Development and,
+          much like Mia Thermopolis, is a yoga doing, horse-back riding, wall
+          climbing type of girl. Amanda is represented by Jenniea Carter at New
+          Leaf Literary Agency.
+        </p>
+      </div>
     </div>
   )
 }
